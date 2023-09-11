b0fba1baf67e572a6df2c522e53616d7f623aed1
artem <sashheka@gmail.com>
2023-09-11 18:20:45 +0300

    optimized generating of leads url

--- methods.py
+++ methods.py
@@ -3,12 +3,14 @@ import json
 
 
 with open('resource/access_token.json') as json_file:
-    access_token = json.load(json_file)["access_token"]
+    data = json.load(json_file)
+    access_token = data.get("access_token")
+    pipeline_id = data.get("pipeline_id")
+
 headers = {
     'Accept': 'application/json',
     'Authorization': f"Bearer {access_token}",
 }
-pipeline_id = 5930839
 
 
 def get_pipeline_statuses_list():
@@ -23,4 +25,8 @@ def get_pipeline_statuses_list():
 
 
 def get_leads_url_filters_by_status_id(pipeline_statuses_list):
+    leads_url_filters_by_status_id = ''
+    enumerated_statuses_dict = dict(enumerate(pipeline_statuses_list))
+    for i, status_id in enumerated_statuses_dict.items():
+        leads_url_filters_by_status_id += f'filter[statuses][{i}][pipeline_id]={pipeline_id}&filter[statuses][{i}][status_id]={status_id}&'
     return leads_url_filters_by_status_id
---
+++ test.py
@@ -0,0 +1,3 @@
+from methods import *
+
+print(get_leads_url_filters_by_status_id(get_pipeline_statuses_list()))
