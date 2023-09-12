3d96ec4e1100235648bf3f9ee031c8e7a7f89792
artem <sashheka@gmail.com>
2023-09-12 12:36:20 +0300

    got dataframe with all necessary fields

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from methods import *
 
 
-pipeline_statuses_list = get_pipeline_statuses_list()
+pipeline_statuses_list = list(get_pipeline_statuses_dict().keys())
 leads_url_filters_by_status_id = get_leads_url_filters_by_status_id(pipeline_statuses_list)
 
 leads_url = f"https://advaga.amocrm.ru/api/v4/leads?{leads_url_filters_by_status_id}page=1&limit=250"
 leads = requests.get(leads_url, headers=headers).json()
+
 with open(f'assets/leads.json', 'w') as f:
     json.dump(leads, f, indent=3)
+
+i = 1
+
+try:
+    while leads['_links']['next']['href']:
+        leads_next_url = leads['_links']['next']['href']
+        leads = requests.get(leads_next_url, headers=headers).json()
+        with open(f'assets/leads_{i}.json', 'w') as f:
+            json.dump(leads, f, indent=3)
+        i += 1
+except KeyError as e:
+    print(e)
--- methods.py
+++ methods.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 
-with open('resource/access_token.json') as json_file:
+with open('resource/access_token.json', 'r') as json_file:
     data = json.load(json_file)
     access_token = data.get("access_token")
     pipeline_id = data.get("pipeline_id")
@@ -13,15 +13,14 @@ headers = {
 }
 
 
-def get_pipeline_statuses_list():
+def get_pipeline_statuses_dict():
     statuses_url = f"https://advaga.amocrm.ru/api/v4/leads/pipelines/{pipeline_id}/statuses"
     statuses = requests.get(statuses_url, headers=headers).json()
     with open(f'assets/statuses.json', 'w') as f:
         json.dump(statuses, f, indent=3)
     statuses_dict = {i['id']: i['name'] for i in statuses['_embedded']['statuses']}
     del statuses_dict[143]
-    statuses_keys_list = list(statuses_dict.keys())
-    return statuses_keys_list
+    return statuses_dict
 
 
 def get_leads_url_filters_by_status_id(pipeline_statuses_list):
